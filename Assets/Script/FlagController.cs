40f4f38f685ccaa9fb78b873edc6c8085f33a5e4
March26th <nguyengation@gmail.com>
2023-07-03 20:55:36 +0700

    BTL Thuc tai ao Haui

---
+++ Assets/Script/FlagController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class FlagController : MonoBehaviour
+{
+    public float amplitude = 0.5f; // Biên độ chuyển động của cờ
+    public float frequency = 1f; // Tần số chuyển động của cờ
+
+    private Vector3 startPos;
+
+    private void Start()
+    {
+        startPos = transform.position;
+    }
+
+    private void Update()
+    {
+        float newY = startPos.y + Mathf.Sin(Time.time * frequency) * amplitude;
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+    }
+}
---
+++ Assets/Script/LanBong.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanBong : MonoBehaviour
+{
+    public float speed = 0;
+    public float x = 0, y = 0, z = 0;
+    public float bandau = 0;
+    private bool ktlan = true, dau = true;
+    private float t = 10;
+    private void FixedUpdate()
+    {
+        float horAxis = Input.GetAxis("Horizontal");
+        float verAxis = Input.GetAxis("Vertical");
+
+        if (bandau == 5 * 144)
+        {
+            ktlan = false;
+            dau = false;
+        }
+
+        if (bandau == 0)
+            ktlan = true;
+
+        if (bandau < 5 * 144 && ktlan == true)
+        {
+            bandau += 1;
+            if (dau)
+                t = 10;
+            else
+                t = 20;
+            z = t;
+        }
+
+        if (bandau > 0 && ktlan == false)
+        {
+            bandau -= 1;
+            z = -20;
+        }
+
+        Vector3 movement = new Vector3(x, y, z);
+
+        GetComponent<Rigidbody>().AddForce(movement * speed * Time.deltaTime);
+    }
+}
